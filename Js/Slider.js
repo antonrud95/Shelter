90f5e4584b7a93afe4fe708ce1def2e0d246729e
Anton Rud <antonrud95@yandex.ru>
2019-12-21 17:44:27 +0200

    Fix/Slider Adaptive

--- Js/Slider.js
+++ Js/Slider.js
@@ -1,7 +1,18 @@
 $('.sl').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false
+      }
+    }
+  ]
 });
 
 
